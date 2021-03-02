0c1ed803e0bc65cfe43663cbc02f72456ab374b4
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-03-02 15:40:26 +0000

    Create lspsetup.lua

---
+++ .config/nvim/lua/lspsetup.lua
@@ -0,0 +1,11 @@
+-- Julia language server
+require'lspconfig'.julials.setup{}
+
+-- Clojure language server
+require'lspconfig'.clojure_lsp.setup{}
+
+-- Jedi language server for Python
+require'lspconfig'.jedi_language_server.setup{}
+
+-- R language server
+require'lspconfig'.r_language_server.setup{}
