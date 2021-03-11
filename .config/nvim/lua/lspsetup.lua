3d7b29d2605517a34391ede64f4ba3636c9d2d27
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-03-11 23:31:31 +0000

    Update lspsetup.lua
    add language server installation instructions (for Linux)

--- .config/nvim/lua/lspsetup.lua
+++ .config/nvim/lua/lspsetup.lua
@@ -1,11 +1,21 @@
--- Julia language server
+-- Language Server configuration
+-- Follow installation notes below before running the neovim LSP.
+
+-- Julia language server - to install:
+-- julia -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer")'
 require'lspconfig'.julials.setup{}
 
--- Clojure language server
+-- Clojure language server - to install:
+-- wget https://github.com/clojure-lsp/clojure-lsp/blob/master/install-latest-clojure-lsp.sh
+-- chmod +x install-latest-clojure-lsp.sh
+-- sudo ./install-latest-clojure-lsp.sh
 require'lspconfig'.clojure_lsp.setup{}
 
--- Jedi language server for Python
-require'lspconfig'.jedi_language_server.setup{}
+-- Language server for Python
+-- npm install -g pyright
+require'lspconfig'.pyright.setup{}
 
--- R language server
+-- R language server - to install:
+-- R
+-- install.packages("languageserver")
 require'lspconfig'.r_language_server.setup{}
