bcc6980a09dbf51031f85c9ccec34a3d2a1c40c9
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-03-11 23:14:38 +0000

    Update options.lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,3 +1,8 @@
+-- neovim options in lua
+-- inspired by https://oroques.dev/notes/neovim-init/
+
+-- functions and variables to set options
+-- equivalent to vimscript's set command
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local function opt(scope, key, value)
@@ -5,32 +10,45 @@ local function opt(scope, key, value)
   if scope ~= 'o' then scopes['o'][key] = value end
 end
 
+-- Put your favorite colour scheme here
+vim.cmd 'colorscheme tender'
+
+-- local buffer options
 local indent = 2
-vim.cmd 'colorscheme tender'              -- Put your favorite colorscheme here
 opt('b', 'expandtab', true)           -- Use spaces instead of tabs
 opt('b', 'shiftwidth', indent)        -- Size of an indent
 opt('b', 'smartindent', true)         -- Insert indents automatically
 opt('b', 'tabstop', indent)           -- Number of spaces tabs count for
 
+-- global options
 opt('o', 'autochdir', true)           -- automatically change directory
 opt('o', 'cmdheight', 2)
 opt('o', 'confirm', true)
 opt('o', 'hidden', true)              -- Enable modified buffers in background
 opt('o', 'ignorecase', true)          -- Ignore case
 opt('o', 'joinspaces', false)         -- No double spaces with join after a dot
-opt('o', 'scrolloff', 4 )             -- Lines of context
+opt('o', 'scrolloff', 4)              -- Lines of context
 opt('o', 'shiftround', true)          -- Round indent
-opt('o', 'shortmess', 'filnxtToOFc')  -- don't give [ins-completion-menu] messages
-opt('o', 'sidescrolloff', 8 )         -- Columns of context
+opt('o', 'sidescrolloff', 8)          -- Columns of context
 opt('o', 'smartcase', true)           -- Don't ignore case with capitals
 opt('o', 'splitbelow', true)          -- Put new windows below current
 opt('o', 'splitright', true)          -- Put new windows right of current
 opt('o', 'termguicolors', true)       -- True color support
-opt('o', 'updatetime', 300)           -- in milliseconds
 opt('o', 'wildmode', 'list:longest')  -- Command-line completion mode
+
+-- local window options
+opt('w', 'foldmethod', 'expr')        -- tree-sitter folding mode
+opt('w', 'foldexpr', 'nvim_treesitter#foldexpr()')
 opt('w', 'list', true)                -- Show some invisible characters (tabs...)
 opt('w', 'number', true)              -- Show line number
 opt('w', 'relativenumber', true)      -- Relative line numbers
 opt('w', 'signcolumn', 'auto')        -- show sign column 
 
+-- Set Python executable binary
 vim.g['python3_host_prog'] = '$HOME/nvim/bin/python3'
+
+-- Set indent-line character
+vim.g['indentLine_char'] = '┆'
+
+-- Rainbow parentheses on
+vim.g['rainbow_active'] = 1
