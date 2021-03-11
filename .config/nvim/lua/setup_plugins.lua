27179621e4de888e9eb3b4a38ac4ed9e6537510b
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-03-11 23:22:03 +0000

    Create setup_plugins.lua
    setup plugins

---
+++ .config/nvim/lua/setup_plugins.lua
@@ -0,0 +1,59 @@
+-- plugin configuration
+
+-- Treesitter - enable modules
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  
+    highlight = {
+    enable = true              -- false will disable the whole extension
+  },
+
+  indent = {
+    enable = true
+  },
+
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false -- Whether the query persists across vim sessions
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+}
+
+-- nvim-compe (completion engine)
+require'compe'.setup {
+    enabled = true;
+    autocomplete = true;
+    debug = false;
+    min_length = 1;
+    preselect = 'enable';
+    throttle_time = 80;
+    source_timeout = 200;
+    incomplete_delay = 400;
+    max_abbr_width = 100;
+    max_kind_width = 100;
+    max_menu_width = 100;
+    documentation = true;
+    source = {
+      path = true;
+      buffer = true;
+      calc = true;
+      vsnip = true;
+      nvim_lsp = true;
+      nvim_lua = true;
+      spell = true;
+      tags = true;
+      snippets_nvim = true;
+      omni = true;
+    };
+  }
