4410e1c678fef1e221a557a0d3d3e07f7f4181e7
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-14 03:53:28 +0000

    Update and rename lspsetup.lua to lsp_setup.lua

---
+++ .config/nvim/lua/lsp_setup.lua
@@ -0,0 +1,96 @@
+-- Language Server configuration
+-- Follow installation notes below before running the neovim LSP.
+
+--[[
+    _        __     ____                                     
+    /      /    )   /    )                                   
+---/-------\-------/____/-------__----__--_/_-------------__-
+  /         \     /            (_ ` /___) /    /   /    /   )
+_/____/_(____/___/____________(__)_(___ _(_ __(___(____/___/_
+                                                      /      
+                                                     /       
+lsp_setup.lua
+
+ASCII art from https://ascii.co.uk/text
+style: Big Chief
+]]--
+
+local nvim_lsp = require 'lspconfig'
+local on_attach = function(_, bufnr)
+vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+-- nvim-cmp (completion engine)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Julia language server - to install:
+-- julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+require'lspconfig'.julials.setup{}
+
+-- Clojure language server - to install:
+-- sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/blob/master/install)
+require'lspconfig'.clojure_lsp.setup{}
+
+-- Language server for Python
+-- npm install -g pyright
+require'lspconfig'.pyright.setup{}
+
+-- R language server - to install:
+-- R
+-- install.packages("languageserver")
+require'lspconfig'.r_language_server.setup{}
+
+local servers = {'julials', 'clojure_lsp', 'pyright', 'r_language_server' }
+for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
