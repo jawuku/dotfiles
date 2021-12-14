ff60dafd57b091b2f2a1e39c25f33b11c0292b8d
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-14 04:58:17 +0000

    Update load_plugins.lua

--- .config/nvim/lua/load_plugins.lua
+++ .config/nvim/lua/load_plugins.lua
@@ -32,9 +32,7 @@ use 'lukas-reineke/indent-blankline.nvim'
 use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
 
 -- nvim telescope (file finder and previewer)
-use {'nvim-telescope/telescope.nvim',
-  requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-}
+use {'nvim-telescope/telescope.nvim',  requires = 'nvim-lua/plenary.nvim'}
 
 use 'nvim-telescope/telescope-fzy-native.nvim'
 
