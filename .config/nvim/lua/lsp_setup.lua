35a9c514a229b2b2aae288f81ca4b5cac983c117
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-15 03:38:29 +0000

    Update lsp_setup.lua

--- .config/nvim/lua/lsp_setup.lua
+++ .config/nvim/lua/lsp_setup.lua
@@ -18,30 +18,31 @@ style: Big Chief
 -- nvim-cmp (completion engine)
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
---[[
+
+local lsp_flags = {
+        on_attach = on_attach,
+        capabilities = capabilities
+}
+    
+
 -- Julia language server - to install:
-julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+-- julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+require'lspconfig'.julials.setup{lsp_flags}
 
 -- Clojure language server - to install:
-sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/blob/master/install)
-or alternatively if using Homebrew: homebrew install clojure-lsp
+-- sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/blob/master/install)
+-- or alternatively if using Homebrew: homebrew install clojure-lsp
+require'lspconfig'.clojure_lsp.setup{lsp_flags}
 
 -- Language server for Python - to install:
-npm install -g pyright
+--npm install -g pyright
+require'lspconfig'.pyright.setup{lsp_flags}
 
 -- R language server - to install:
-R
-install.packages("languageserver")
+-- R
+-- install.packages("languageserver")
+require'lspconfig'.r_language_server.setup{lsp_flags}
 
-]]--
-
-local servers = {'julials', 'clojure_lsp', 'pyright', 'r_language_server' }
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
-end
 
 -- luasnip setup
 local luasnip = require 'luasnip'
