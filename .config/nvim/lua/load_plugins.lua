5a10feb741dc4fb66a9396083abeed4171a3cc6d
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-15 01:55:03 +0000

    add installation of packer.nvim all into this file

--- .config/nvim/lua/load_plugins.lua
+++ .config/nvim/lua/load_plugins.lua
@@ -10,7 +10,13 @@ load-plugins.lua
 ASCII art from https://ascii.co.uk/text
 style: Big Chief							 
 ]]--
-return require('packer').startup(function()
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
 
 -- status bar
 -- use 'ojroques/nvim-hardline'
@@ -64,4 +70,8 @@ use 'equalsraf/neovim-gui-shim'
 -- autoclose pairs
 use 'Raimondi/delimitMate'
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+        
 end)
