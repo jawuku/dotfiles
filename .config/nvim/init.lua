970be265ef4832b50644faa9a9cb7ecd48f392f7
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-15 01:48:34 +0000

    move packer.nvim installation to load_plugins
    and attribution to Olivier Roques

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,30 +7,17 @@
 : ##:: ##:. ###:: ##::::: ##:::::::'###:::: ##::::::: ##:::: ##: ##.... ##:
 '####: ##::. ##:'####:::: ##::::::: ###:::: ########:. #######:: ##:::: ##:
 ....::..::::..::....:::::..::::::::...:::::........:::.......:::..:::::..::
-init.lua for Neovim v0.5+
-A lot of code comes from github.com/nvim-lua/kickstart.nvim
+init.lua for Neovim v0.6+
+A lot of code and ideas come from github.com/nvim-lua/kickstart.nvim
+also from Olivier Roque's webpage https://oroques.dev/notes/neovim-init/
 
 ASCII art from https://ascii.co.uk/text
 style: Banner3d
 ]]--
 
--- Automatically install packer.nvim
-local cmd = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  cmd('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  cmd 'packadd packer.nvim'
-end
-
--- load config files
+-- Load config files
 require('load_plugins')
 require('setup_plugins')
 require('options')
 require('keymaps')
 require('lsp_setup')
-
--- lazy load packer.nvim plugins
-cmd('autocmd BufWritePost plugins.lua PackerCompile')
