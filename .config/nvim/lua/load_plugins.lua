5a9f67716fb595daedf9f44629ce03d971e2827d
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-23 13:35:49 +0000

    added nvim-cmp plugins

--- .config/nvim/lua/load_plugins.lua
+++ .config/nvim/lua/load_plugins.lua
@@ -58,8 +58,11 @@ use 'nvim-telescope/telescope-fzy-native.nvim'
 use 'neovim/nvim-lspconfig'
 
 -- autocompletion support
-use 'hrsh7th/nvim-cmp'
 use 'hrsh7th/cmp-nvim-lsp'
+use 'hrsh7th/cmp-buffer'
+use 'hrsh7th/cmp-path'
+use 'hrsh7th/cmp-cmdline'
+use 'hrsh7th/nvim-cmp'
 
 -- manage code snippets
 use 'saadparwaiz1/cmp_luasnip'
