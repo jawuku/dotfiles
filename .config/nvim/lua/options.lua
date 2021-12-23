9c9d1b2d28d9f6c6b1df767086de7cd2d4c4e543
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-12-23 13:03:45 +0000

    new easier config style. New colourscheme darcula

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -11,65 +11,58 @@ ASCII art from https://ascii.co.uk/text
 style: Big Chief	   
 ]]--
 
-local g = vim.g
-local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
-local opt = vim.opt
-local cmd = vim.cmd
+-- new config style inspired by Christian Chiarulli's options page
+-- https://github.com/ChristianChiarulli/nvim/blob/master/lua/user/options.lua
 
--- local buffer options --
-local indent = 4
-bo.expandtab = true          -- Use spaces instead of tabs
-bo.modeline = false          -- don't display mode, as status bar does this
-bo.shiftwidth = indent       -- Size of an indent
-bo.smartindent = true        -- Insert indents automatically
-bo.tabstop = indent          -- Number of spaces tabs count for
+local myoptions = {
+  expandtab = true,          -- Use spaces instead of tabs
+  modeline = false,          -- don't display mode, as status bar does this
+  shiftwidth = indent,       -- Size of an indent
+  smartindent = true,        -- Insert indents automatically
+  tabstop = indent,          -- Number of spaces tabs count for
+  autochdir = true,          -- automatically change directory
+  clipboard = 'unnamed,unnamedplus', -- clipboard integrates with desktop
+  cmdheight = 2,             -- allows displaying of error messages below 
+  confirm = true,            -- Confirm to save file on exit
+  hidden = true,             -- Enable modified buffers in background
+  ignorecase = true,         -- Ignore case
+  joinspaces = false,        -- No double spaces with join after a dot
+  scrolloff = 4,             -- Lines of context
+  shiftround = true,         -- Round indent
+  showmode = false,          -- remove --INSERT-- notifications
+  sidescrolloff = 8,         -- Columns of context
+  smartcase = true,          -- Don't ignore case with capitals
+  splitbelow = true,         -- Put new windows below current
+  splitright = true,         -- Put new windows right of current
+  termguicolors = true,      -- True color support
+  wildmode = 'list:longest', -- Command-line completion mode
+  foldmethod = 'expr',       -- tree-sitter folding mode
+  foldexpr = 'nvim_treesitter#foldexpr()',
+  list = true,               -- Show some invisible characters (tabs...)
+  number = true,             -- Show line number
+  relativenumber = true,     -- Relative line numbers
+  signcolumn = 'auto',       -- show sign column 
+  undofile = true,
+  background = 'dark',
+  }
 
--- global options --
-o.autochdir = true           -- automatically change directory
-o.clipboard = 'unnamed,unnamedplus' -- clipboard integrates with desktop
-o.cmdheight = 2              -- allows displaying of error messages below 
-o.confirm = true             -- Confirm to save file on exit
-o.hidden = true              -- Enable modified buffers in background
-o.ignorecase = true          -- Ignore case
-o.joinspaces = false         -- No double spaces with join after a dot
-o.scrolloff = 4              -- Lines of context
-o.shiftround = true          -- Round indent
-o.sidescrolloff = 8          -- Columns of context
-o.smartcase = true           -- Don't ignore case with capitals
-o.splitbelow = true          -- Put new windows below current
-o.splitright = true          -- Put new windows right of current
-o.termguicolors = true       -- True color support
-o.wildmode = 'list:longest'  -- Command-line completion mode
-
--- local window options --
-wo.foldmethod = 'expr'       -- tree-sitter folding mode
-wo.foldexpr = 'nvim_treesitter#foldexpr()'
-wo.list = true               -- Show some invisible characters (tabs...)
-wo.number = true             -- Show line number
-wo.relativenumber = true     -- Relative line numbers
-wo.signcolumn = 'auto'       -- show sign column 
-
--- save undo history
-opt.undofile = true
-
--- set background as 'dark' or 'light'
-o.background = 'dark'
+for k, v in pairs(myoptions) do
+  vim.opt[k] = v
+end
 
 -- Put your favorite colour scheme here
--- Choices: tender, NeoSolarized
-vim.cmd "colorscheme tender"
+-- Choices: tender, darcula, NeoSolarized
+vim.cmd 'colorscheme darcula'
 
 -- Set Python executable binary
 -- adjust to your Python executable location
-g.python3_host_prog = '$HOME/mambaforge/envs/datasci/bin/python3'
+vim.g.python3_host_prog = '$HOME/mambaforge/envs/datasci/bin/python3'
 
--- tenderplus colour scheme for lightline
-g.lightline = { colorscheme = 'tenderplus' }
+-- darcula colour scheme for lightline
+vim.g.lightline = { colorscheme = 'darculaOriginal' }
 
 -- Set indent-line character, exclude it showing in help files and terminal
-g.indent_blankline_char = '┆'
-g.indent_blankline_filetype_exclude = { 'help', 'packer'}
-g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
-g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_char = '┆'
+vim.g.indent_blankline_filetype_exclude = { 'help', 'packer'}
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile'}
+vim.g.indent_blankline_show_trailing_blankline_indent = false
