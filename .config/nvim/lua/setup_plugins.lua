f9efe30b53aa4b1cf5582bc77148e77c10435806
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-07-24 17:16:48 +0100

    ensure_installed now a list of parsers
    "maintained" option removed 30 Apr 2022
    see https://github.com/nvim-treesitter/nvim-treesitter/issues/2293

--- .config/nvim/lua/setup_plugins.lua
+++ .config/nvim/lua/setup_plugins.lua
@@ -13,7 +13,7 @@ style: Big Chief
 
 -- Treesitter - enable modules
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"lua", "python", "julia", "r", "clojure"}, -- one of "all", or a list of languages
   
     highlight = {
     enable = true              -- false will disable the whole extension
