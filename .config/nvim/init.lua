8eded7f78e04a46de42c06d4f268464ebd87aec9
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-08-12 23:56:56 +0100

    reflect namespaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,20 +7,22 @@
 : ##:: ##:. ###:: ##::::: ##:::::::'###:::: ##::::::: ##:::: ##: ##.... ##:
 '####: ##::. ##:'####:::: ##::::::: ###:::: ########:. #######:: ##:::: ##:
 ....::..::::..::....:::::..::::::::...:::::........:::.......:::..:::::..::
-init.lua for Neovim v0.6+
+init.lua for Neovim v0.7+
 A lot of code and ideas come from github.com/nvim-lua/kickstart.nvim
 and Christian Chiarulli https://github.com/LunarVim/Neovim-from-scratch
+and Jake Wiesler's YouTube videos (esp. Lua namespaces)
+
 ASCII art from https://ascii.co.uk/text
 style: Banner3d
 ]]--
 
-require('load_plugins')
+require('user.options')
+require('user.load_plugins')
 
 -- Put your favorite colour scheme here
 -- Choices: tender, darcula, NeoSolarized
-vim.cmd 'colorscheme darcula'
+vim.cmd 'colorscheme darkplus'
 
-require('setup_plugins')
-require('options')
-require('keymaps')
-require('lsp_setup')
+require('user.setup_plugins')
+require('user.keymaps')
+require('user.lsp_cmp_setup')
