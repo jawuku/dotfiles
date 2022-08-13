a8633e97485a70cd62756d1ae04648fc623400e0
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-08-13 12:43:39 +0100

    add neovim config

--- home.nix
+++ home.nix
@@ -9,6 +9,38 @@
     kitty
     htop
     most
+    neovim
+    tree-sitter
+    rPackages.languageserver
+    rPackages.tidyverse
+    rPackages.devtools
+    rPackages.IRkernel
+	  rPackages.ggplot2
+	  rPackages.lintr
+	  rPackages.styler
+	  python39
+	  python39Packages.sympy
+	  python39Packages.seaborn
+	  python39Packages.notebook
+	  python39Packages.numpy
+	  python39Packages.matplotlib
+	  python39Packages.scikit-learn
+	  python39Packages.pandas
+	  python39Packages.scipy
+	  python39Packages.gmpy2
+	  python39Packages.pillow-simd
+	  nodePackages.pyright
+	  nodePackages.bash-language-server
+	  # julia_17-bin # only for x86_64
+	  clojure
+	  clojure-lsp
+	  leiningen
+	  clang
+	  clang-tools
+	  octaveFull
+	  fzf
+	  ripgrep
+	  sumneko-lua-language-server
   ];
   
   programs.bat.enable = true;
@@ -19,9 +51,10 @@
     enableSyntaxHighlighting = true;
     
     shellAliases = {
+      cat = "bat";
       ls  = "exa";
       ll  = "exa -la --icons";
-      cat = "bat";
+      lt = "exa --tree";
       update  = "sudo nix-channel update";
       upgrade = "sudo nixos-rebuild switch";
     };
@@ -31,6 +64,46 @@
       TERM = "kitty";
       PAGER = "most";
     };
+  };
+  
+  programs.neovim = {
+    enable = true;
+    vimAlias = false;
+
+	  plugins = with pkgs.vimPlugins; [
+	    nvim-lspconfig
+	    cmp-nvim-lsp
+	    cmp-buffer
+	    cmp-path
+	    cmp-cmdline
+	    nvim-cmp
+	    nvim-web-devicons
+	    lualine-nvim
+	    bufferline-nvim
+	    comment-nvim
+	    nvim-treesitter
+	    nvim-ts-rainbow
+	    indent-blankline-nvim
+	    plenary-nvim
+	    telescope-nvim
+	    telescope-fzy-native
+	    cmp_luasnip
+	    luasnip
+	    tender-vim
+	    NeoSolarized
+	    nvim-autopairs
+	    vim-code-dark
+	  ];
+	
+	  extraConfig = ''
+	    lua << EOF
+	    ${builtins.readFile /home/jason/.config/nvim/user/setup_plugins.lua}
+		  ${builtins.readFile /home/jason/.config/nvim/user/options.lua}
+		  ${builtins.readFile /home/jason/.config/nvim/user/keymaps.lua}
+		  ${builtins.readFile /home/jason/.config/nvim/user/lsp_setup.lua}
+	    EOF
+	    colorscheme codedark
+	  '';
   };
   
   programs.kitty = {
