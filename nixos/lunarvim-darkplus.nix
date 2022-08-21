6d1054674161629809ce574d0b78a86914fbbeeb
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-08-21 15:39:27 +0100

    change sha256 to proper value shown by error

--- nixos/lunarvim-darkplus.nix
+++ nixos/lunarvim-darkplus.nix
@@ -11,5 +11,6 @@ pkgs.vimUtils.buildVimPlugin {
     repo = "darkplus.nvim";
     rev = "49cfa2b2aaea0389436e6bc220a92c998249d10f";
     sha256 = "1c2spdx4jvv7j52f37lxk64m3rx7003whjnra3y1c7m2d7ljs6rb";
+    # change to the value suggested by the error
   };
 }
