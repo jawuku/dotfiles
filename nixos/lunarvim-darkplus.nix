e47370209cf62d59af13432009d2e1ca783618d9
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-08-21 13:44:12 +0100

    Lunarvim darkplus colourscheme

---
+++ nixos/lunarvim-darkplus.nix
@@ -0,0 +1,15 @@
+# darkplus colourscheme for Neovim written in Lua
+# from https://github.com/LunarVim/darkplus.nvim
+
+{ pkgs, fetchFromGitHub }:
+
+pkgs.vimUtils.buildVimPlugin {
+  pname = "lunarvim-darkplus";
+
+  src = fetchFromGitHub {
+    owner = "LunarVim";
+    repo = "darkplus.nvim";
+    rev = "49cfa2b2aaea0389436e6bc220a92c998249d10f";
+    sha256 = "1c2spdx4jvv7j52f37lxk64m3rx7003whjnra3y1c7m2d7ljs6rb";
+  };
+}
