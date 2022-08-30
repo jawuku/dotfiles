10aa9e0ed7d2435fde36b7feda846648ae1d9e9f
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-08-30 19:48:43 +0100

    Update init.lua

--- .config/nvim/lua/cmp-config/init.lua
+++ .config/nvim/lua/cmp-config/init.lua
@@ -84,6 +84,72 @@ style: Default Font
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-    capabilities = capabilities
-  }
+  --require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+  --  capabilities = capabilities
+  --}
+--[[ Julia language server - to install:
+julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+where ~/.julia/environments/nvim-lspconfig is the location where the default configuration expects LanguageServer.jl to be installed.
+To update an existing install, use the following command:
+julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()'
+]]--
+require'lspconfig'.julials.setup{lsp_flags}
+
+-- Clojure language server - to install:
+-- sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/blob/master/install)
+-- or alternatively if using Homebrew: brew install clojure-lsp/brew/clojure-lsp-native
+-- or if on NixOS install the package clojure-lsp
+require'lspconfig'.clojure_lsp.setup{lsp_flags}
+
+-- Language server for Python - to install:
+-- npm install -g pyright
+-- or if on NixOS install the package nodePackages.pyright
+require'lspconfig'.pyright.setup{lsp_flags}
+
+-- R language server - to install:
+-- R
+-- install.packages("languageserver")
+-- or if on NixOS install the package rPackages.languageserver
+require'lspconfig'.r_language_server.setup{lsp_flags}
+
+-- bash language server
+-- on NixOS install package nodePackages.bash-language-server
+-- otherwise: npm install -g bash-language-server
+require'lspconfig'.bashls.setup{lsp_flags}
+
+--[[ Lua language server - to install:
+ if using homebrew (easy) : brew install lua-language-server
+ NixOS : install the package sumneko-lua-language-server
+ 
+-- otherwise (more involved)
+git clone https://github.com/sumneko/lua-language-server
+cd lua-language-server
+git submodule update --init --recursive
+cd 3rd/luamake
+./compile/install.sh
+cd ../..
+./3rd/luamake/luamake rebuild
+sudo ln -s ~/lua-language-server/bin/lua-language-server /usr/local/bin/lua-language-server
+]]--
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
