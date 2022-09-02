7c7610296e0ba1f3e6a2e6c16a7c68a0ca88c943
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-09-02 05:01:00 +0100

    Update init.lua

--- .config/nvim/lua/cmp-config/init.lua
+++ .config/nvim/lua/cmp-config/init.lua
@@ -61,7 +61,6 @@ style: Default Font
       -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
       before = function (entry, vim_item)
-        ...
         return vim_item
       end
       })
