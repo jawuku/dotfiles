4a1892e7c9538897a9d18886259bfa0eb6990d9b
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-09-08 23:10:06 +0100

    Update init.lua

--- .config/nvim/lua/lsp-zero/init.lua
+++ .config/nvim/lua/lsp-zero/init.lua
@@ -1,3 +1,17 @@
+--[[
+88                                                                       
+88                                                                       
+88                                                                       
+88 ,adPPYba, 8b,dPPYba,     888888888  ,adPPYba, 8b,dPPYba,  ,adPPYba,   
+88 I8[    "" 88P'    "8a         a8P" a8P_____88 88P'   "Y8 a8"     "8a  
+88  `"Y8ba,  88       d8      ,d8P'   8PP""""""" 88         8b       d8  
+88 aa    ]8I 88b,   ,a8"    ,d8"      "8b,   ,aa 88         "8a,   ,a8"  
+88 `"YbbdP"' 88`YbbdP"'     888888888  `"Ybbd8"' 88          `"YbbdP"'   
+             88                                                          
+             88                                                          
+ASCII Art from https://www.ascii.co.uk/text
+Default Font
+]]
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
