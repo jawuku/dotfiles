3581fb70426a570430a11bda580ee2500cb22e98
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-09-08 22:24:21 +0100

    Update init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -83,19 +83,30 @@ use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
 -- Which-key : configure keybindings
 use "folke/which-key.nvim"
 
--- Nvim-cmp : autocompletion
-use "neovim/nvim-lspconfig"
-use "hrsh7th/cmp-nvim-lsp"
-use "hrsh7th/cmp-buffer"
-use "hrsh7th/cmp-path"
-use "hrsh7th/cmp-cmdline"
-use "hrsh7th/nvim-cmp"
+-- Lsp-Zero
+use {
+   "VonHeikemen/lsp-zero.nvim",
+  requires = {
+  -- LSP Support    
+  {"neovim/nvim-lspconfig"},
+  {"williamboman/mason.nvim"},
+  {"williamboman/mason-lspconfig.nvim"},
 
--- Snippets
-use "L3MON4D3/LuaSnip"
-use "saadparwaiz1/cmp_luasnip"
-use "rafamadriz/friendly-snippets"
-  
+  -- Nvim-cmp : autocompletion
+  {"hrsh7th/nvim-cmp"},
+  {"hrsh7th/cmp-buffer"},
+  {"hrsh7th/cmp-path"},
+  {"hrsh7th/cmp-cmdline"},
+  {"hrsh7th/cmp-nvim-lsp"},
+  {"hrsh7th/cmp-nvim-lua"},
+
+  -- Snippets
+  {"L3MON4D3/LuaSnip"},
+  {"saadparwaiz1/cmp_luasnip"},
+  {"rafamadriz/friendly-snippets"},
+  }
+}
+
 -- Display completion icons
 use "onsails/lspkind.nvim"
 
