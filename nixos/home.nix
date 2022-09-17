2710f9b7efe9a11a60032e2689415968852c45ea
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2022-09-17 02:20:57 +0100

    add gruvbox theme to kitty config

--- nixos/home.nix
+++ nixos/home.nix
@@ -83,6 +83,35 @@ imports = [ ./neovim.nix ];
       remember_window_size = true;
       initial_window_width  = "80c";
       initial_window_height = "25c";
+      
+      # Gruvbox theme
+      selection_foreground = "#ebdbb2";
+      selection_background = "#d65d0e";
+      
+      foreground           = "#ebdbb2";
+      background           = "#282828";
+      
+      color0               = "#3c3836";
+      color1               = "#cc241d";
+      color2               = "#98971a";
+      color3               = "#d79921";
+      color4               = "#458588";
+      color5               = "#b16286";
+      color6               = "#689d6a";
+      color7               = "#a89984";
+      color8               = "#928374";
+      color9               = "#fb4934";
+      color10              = "#b8bb26";
+      color11              = "#fabd2f";
+      color12              = "#83a598";
+      color13              = "#d3869b";
+      color14              = "#83c07c";
+      color15              = "#fbf1c7";
+      
+      cursor               = "#bdae93";
+      cursor_text_color    = "#665c54";
+      
+      url_color            = "#458588";
     };
   };
 }
