194f9bc57487d65ba38c7e5b4b9ed7eee02047de
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2023-02-07 21:52:44 +0000

    Update distrobox-arch-postinstall.sh

--- os_setups/distrobox-arch-postinstall.sh
+++ os_setups/distrobox-arch-postinstall.sh
@@ -15,23 +15,9 @@ sudo printf "en_GB.UTF-8 UTF-8\ncy_GB.UTF-8 UTF-8\n" > /etc/locale.gen
 sudo locale-gen
 
 # utilities
-sudo pacman -S base-devel git neofetch
-
-## install extra utilities not installed on host system
-## to avoid confusion
-
-# Falkon - lightweight web browser
-sudo pacman -S falkon
-
-# install Wezterm with openGL libraries
-sudo pacman -S wezterm mesa
-wget https://raw.githubusercontent.com/jawuku/dotfiles/master/.wezterm.lua
-
-# install micro  and neovim text editors
-sudo pacman -S micro neovim
-
-# extra neovim tools
-sudo pacman -S python-pynvim python-setuptools xclip
+sudo pacman -S git helix base-devel jdk11-openjdk intellij-idea-community-edition \
+ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
+bat exa neofetch
 
 # install zsh
 sudo pacman -S zsh zsh-autosuggestions zsh-syntax-highlighting
@@ -39,15 +25,16 @@ sudo pacman -S zsh zsh-autosuggestions zsh-syntax-highlighting
 # change default shell to zsh
 sudo chsh -s $(which zsh) $USER
 
+# download .zshrc
+curl https://raw.githubusercontent.com/jawuku/dotfiles/master/.zshrc-archlinux -o $HOME/.zshrc
+sed -i "s/yourname/$USER/g" $HOME/.zshrc
+
 # install paru, an Archlinux AUR installer
-mkdir $HOME/aur
-cd $HOME/aur
+mkdir $HOME/AUR
+cd $HOME/AUR
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
-
-# Install Fira Code Nerd Font
-paru -S nerd-fonts-fira-code
 
 # install Sumneko Lua language server
 paru -S lua-language-server-git
@@ -57,20 +44,30 @@ sudo pacman -S bash-language-server shellcheck bash-completion
 paru -S beautysh
 
 # install Clojure, Clojure LSP and linters
-sudo pacman -S jdk11-openjdk
 sudo pacman -S leiningen clojure rlwrap
 lein version # downloads Leiningen binary
+paru -S clojure-lsp-bin
 
-paru -S cljstyle-bin clj-kondo-bin
+# paru -S cljstyle-bin clj-kondo-bin
 
 # install Python utils
-sudo pacman -S python-seaborn python-statsmodels python-numpy openblas
-sudo pacman -S python-sympy python-gmpy2
-sudo pacman -S jupyter-notebook
-sudo pacman -S pyright # Python language server
-sudo pacman -S python-flake8-black python-flake8-isort
+sudo pacman -S python-seaborn python-statsmodels openblas python-sympy python-gmpy2 \
+jupyter-notebook python-lsp server python-lsp-black python-pip python-wheel
+
+# Scala Tools with Metals Language Server
+paru -S coursier
+coursier setup
+coursier install metals
+
+# Install Julia
+pip3 install jill --user -U
+$HOME/.local/bin/julia -e 'Using Pkg; Pkg.add(["IJulia", "Plots", "OhMyREPL", "PackageCompiler", "RowEchelon", "LanguageServer", "Symbolics"])'
 
 # install R
 sudo pacman -S r gcc-fortran tk
 
 Rscript -e "install.packages( c('devtools', 'languageserver', 'IRkernel', 'tidyverse'), dependencies = TRUE, repos = 'https://cran.ma.imperial.ac.uk')"
+
+# RISC_V Simulators
+sudo pacman -S gtk3 libxtst
+paru -S rars
