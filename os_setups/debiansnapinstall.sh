260843f44b74a804f2d4aa2c751ce6cdaccdc851
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2023-12-04 02:53:59 +0000

    Update debiansnapinstall.sh

--- os_setups/debiansnapinstall.sh
+++ os_setups/debiansnapinstall.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 # debloat Debian 12 Gnome installed from tasksel
 # remove debs and replace with snaps
 
+cd ~
 sudo apt update
 sudo apt -y upgrade
 
 sudo apt -y install snapd
 sudo snap install core
 
-sudo apt install build-essential git curl fonts-noto podman
+sudo apt install build-essential git curl fonts-noto podman rlwrap
 
 sudo apt -y purge firefox-esr aisleriot gnome-2048 gnome-calculator \
 gnome-characters gnome-calendar gnome-characters cheese gnome-chess \
@@ -21,7 +23,7 @@ gnome-klotski eog gnome-mines gnome-music gnome-system-monitor libreoffice*
 sudo apt -y autoremove
 
 declare -a snaps=(
-    "eog"
+    "loupe"
     "firefox"
     "gnome-calculator"
     "gnome-calendar"
@@ -43,6 +45,8 @@ declare -a snaps=(
     "snap-store"
     "gtkhash"
     "bitwarden"
+    "openjdk"
+    "racket"
 )
 
 declare -a classic_snaps=(
@@ -64,12 +68,22 @@ do
     sudo snap install --classic $i
 done
 
-# install special distrobox snap
-sudo snap install distrobox --edge --devmode
+# setup openjdk snap
+# from https://github.com/jgneff/openjdk
+echo "export JAVA_HOME=/snap/openjdk/current/jdk" >> ~/.bashrc
+
+# install latest distrobox
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+# install clojure
+curl -L -O https://github.com/clojure/brew-install/releases/latest/download/posix-install.sh
+chmod +x posix-install.sh
+sudo ./posix-install.sh
+rm posix-install.sh
 
 # Download and install FiraCode Nerd Fonts
 cd ~/Downloads
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
 
 mkdir -p ~/.local/share/fonts
 
