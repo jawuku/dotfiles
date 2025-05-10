4cdb36be056857cb9107e681b8629901a2a08dd4
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2025-05-10 13:53:40 +0100

    Update versions, add new tools

--- os_setups/debiansnapinstall.sh
+++ os_setups/debiansnapinstall.sh
@@ -9,7 +9,8 @@ sudo apt -y upgrade
 sudo apt -y install snapd
 sudo snap install snapd
 
-sudo apt -y install build-essential git curl fonts-noto podman rlwrap
+sudo apt -y install build-essential git curl fonts-noto podman rlwrap \
+openjdk-21-jdk shellcheck shfmt
 
 sudo apt -y purge firefox-esr aisleriot gnome-2048 gnome-calculator \
 gnome-characters gnome-calendar gnome-characters cheese gnome-chess \
@@ -44,15 +45,15 @@ declare -a snaps=(
     "snap-store"
     "gtkhash"
     "bitwarden"
-    "openjdk"
     "racket"
 )
 
 declare -a classic_snaps=(
-    "pycharm-community"
-    "intellij-idea-community"
+    "code"
     "helix"
     "julia"
+    "bash-language-server"
+    "astral-uv"
 )
 
 # install snaps
@@ -67,30 +68,30 @@ do
     sudo snap install --classic $i
 done
 
-# setup openjdk snap
-# from https://github.com/jgneff/openjdk
-printf "\n# OpenJDK Paths\nexport JAVA_HOME=/snap/openjdk/current/jdk\n" >> ~/.bashrc
-echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bashrc
-export JAVA_HOME=/snap/openjdk/current/jdk
-export PATH=$JAVA_HOME/bin:$PATH
-
 # install latest distrobox
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
-# install clojure
+# install Clojure CLI tools
 cd ~/Downloads
 curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
 chmod +x linux-install.sh
 sudo ./linux-install.sh
 rm linux-install.sh
 
+# Leiningen
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 sudo mv ./lein /usr/local/bin
 sudo chmod +x /usr/local/bin/lein
 lein
 
+# Clojure LSP
+sudo bash < <(curl -s https://raw.githubusercontent.com/clojure-lsp/clojure-lsp/master/install)
+
+# Clojure formatter
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/weavejester/cljfmt/HEAD/install.sh)"
+
 # NodeJS
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -102,8 +103,8 @@ echo "NodeJS $(node -v) now installed"
 
 # Download and install Nerd Fonts
 cd ~/Downloads
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.tar.xz
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/RobotoMono.tar.xz
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.tar.xz
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/RobotoMono.tar.xz
 mkdir -p ~/.local/share/fonts
 
 tar xvf FiraCode.xz --wildcards "FiraCodeNerdFont-*.ttf"
