5c7c1c2be922b5e8b62f52e54f4fd12d67193dca
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2025-05-13 13:30:11 +0100

    Update distrobox-arch-postinstall.sh

--- os_setups/distrobox-arch-postinstall.sh
+++ os_setups/distrobox-arch-postinstall.sh
@@ -15,9 +15,8 @@ sudo printf "en_GB.UTF-8 UTF-8\ncy_GB.UTF-8 UTF-8\n" > /etc/locale.gen
 sudo locale-gen
 
 # utilities
-sudo pacman -S git helix base-devel jdk11-openjdk intellij-idea-community-edition \
-ttf-firacode-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
-bat exa neofetch
+sudo pacman -S git helix base-devel jdk11-openjdk code \
+bat eza fastfetch cpufetch
 
 # install zsh
 sudo pacman -S zsh zsh-autosuggestions zsh-syntax-highlighting
@@ -36,40 +35,42 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
 
-# install Sumneko Lua language server
-paru -S lua-language-server-git
+# install Lua language server
+sudo pacman -S lua-language-server stylua
 
 # Install Bash language server and formatter
-sudo pacman -S bash-language-server shellcheck bash-completion
-paru -S beautysh
+sudo pacman -S bash-language-server shellcheck bash-completion shfmt
 
 # install Clojure, Clojure LSP and linters
 sudo pacman -S leiningen clojure rlwrap
 lein version # downloads Leiningen binary
-paru -S clojure-lsp-bin
 
-# paru -S cljstyle-bin clj-kondo-bin
+paru -S clojure-lsp-bin cljfmt-bin
 
-# install Python utils
-sudo pacman -S python-seaborn python-statsmodels openblas python-sympy python-gmpy2 \
-jupyter-notebook python-lsp server python-lsp-black python-pip python-wheel
+# install Python utils and create environment
+sudo pacman -S uv
+cd ~
+uv init --python 3.12 datasci
+cd datasci
+rm pyproject.toml
+wget https://raw.githubusercontent.com/jawuku/dotfiles/refs/heads/master/pyproject.toml
+uv sync
 
 # Scala Tools with Metals Language Server
-paru -S coursier
+paru -S coursier-bin
 coursier setup
 coursier install metals
 
 # Install Julia
-pip3 install jill --user -U
-$HOME/.local/bin/julia -e 'Using Pkg; Pkg.add(["IJulia", "Plots", "OhMyREPL", "PackageCompiler", "RowEchelon", "LanguageServer", "Symbolics"])'
+sudo pacman -S julia
+julia -e 'Using Pkg; Pkg.add(["IJulia", "Plots", "OhMyREPL", "PackageCompiler", "RowEchelon", \
+"LanguageServer", "Symbolics"])'
 
 # install R
 sudo pacman -S r gcc-fortran tk
 
-Rscript -e "install.packages( c('devtools', 'languageserver', 'IRkernel', 'tidyverse'), dependencies = TRUE, repos = 'https://cran.ma.imperial.ac.uk')"
+Rscript -e "install.packages( c('devtools', 'languageserver', 'IRkernel', 'tidyverse'), \
+dependencies = TRUE, repos = 'https://cran.ma.imperial.ac.uk')"
 
 # RISC_V Simulator
-sudo pacman -S fuse2
-cd $HOME/Downloads
-wget https://github.com/mortbopet/Ripes/releases/download/v2.2.6/Ripes-v.2.2.6-linux-x86_64.Appimage
-chmod a+x Ripes-v.2.2.6-linux-x86_64.Appimage
+paru -S ripes-bin
